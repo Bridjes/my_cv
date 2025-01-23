2341504f5a6f4f6a4ee4c1a7b5890c8c27561009
Bridjes <iluha.murzhin@gmail.com>
2025-01-23 20:51:31 +0300

    fix download button

--- assets/js/download_file.js
+++ assets/js/download_file.js
@@ -2,7 +2,7 @@ function downloadWordFile() {
     const fileName = 'assets/documents/Murzhyn_Ilya.pdf'; // Путь к файлу относительно HTML-файла
     const link = document.createElement('a');
     link.href = fileName;
-    link.download = 'example.docx'; // Имя, под которым файл будет сохранен
+    link.download = `${fileName}`; // Имя, под которым файл будет сохранен
 
     // Добавляем элемент в DOM
     document.body.appendChild(link);
