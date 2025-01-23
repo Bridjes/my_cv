5c746a2e4c1afe1b11cf7ede190cae1943b3fee2
Bridjes <iluha.murzhin@gmail.com>
2025-01-23 20:47:41 +0300

    add download button and fix text

---
+++ assets/js/download_file.js
@@ -0,0 +1,15 @@
+function downloadWordFile() {
+    const fileName = 'assets/documents/Murzhyn_Ilya.pdf'; // Путь к файлу относительно HTML-файла
+    const link = document.createElement('a');
+    link.href = fileName;
+    link.download = 'example.docx'; // Имя, под которым файл будет сохранен
+
+    // Добавляем элемент в DOM
+    document.body.appendChild(link);
+
+    // Имитируем клик по ссылке
+    link.click();
+
+    // Удаляем элемент из DOM
+    document.body.removeChild(link);
+}
